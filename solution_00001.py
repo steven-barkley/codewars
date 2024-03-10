93f58436a48d945b2acb6f2249d1c66f122ba2a2
Steven Reginald Barkley <steven.barkley@outlook.com>
2024-03-10 18:39:39 -0400

    Create solution_00001.py
    Added first Codewars solution

---
+++ solution_00001.py
@@ -0,0 +1,13 @@
+def open_or_senior(data):
+    # Handicap 0 to 28 
+    # Seniors >=55 and Handicap > 7 
+    # All others are Open 
+    
+    # output = []
+    
+    # Run a for loop on data (which is a nested list)
+    # if i[0][0] >= 55 and i[0][1] > 7:
+    #     output += "Senior"
+    # else:
+    #     output += "Open"
+    return
